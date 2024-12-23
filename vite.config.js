12924dfd0fe61adc4961793a682b854a4543b719
Haruna Lawali <lawaliharuna943@gmail.com>
2024-12-23 10:29:22 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  define: {
-    'import.meta.env': process.env
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    define: {
+      'import.meta.env.VITE_PAYSTACK_SECRET_KEY': JSON.stringify(env.VITE_PAYSTACK_SECRET_KEY),
+    },
+  };
 });
