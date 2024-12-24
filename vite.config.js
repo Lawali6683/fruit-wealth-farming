f0c49e81c8bcc070c210df4cb7d3c0a9220a3aa3
Haruna Lawali <lawaliharuna943@gmail.com>
2024-12-24 13:04:59 +0100

    Update vite.config.js
    Signed-off-by: Haruna Lawali  <lawaliharuna943@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-
+  
   return {
     define: {
-      'process.env.VITE_PAYSTACK_SECRET_KEY': JSON.stringify(env.VITE_PAYSTACK_SECRET_KEY || ''),
+      'process.env.PAYSTACK_SECRET_KEY': JSON.stringify(env.PAYSTACK_SECRET_KEY || ''),
     },
   };
 });
