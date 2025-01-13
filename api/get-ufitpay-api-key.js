c4e665a7faf7d4c5ee428bb2000bc9a873f1a8e7
Haruna Lawali <lawaliharuna943@gmail.com>
2025-01-13 20:04:48 +0100

    Update get-ufitpay-api-key.js
    Signed-off-by: Haruna Lawali  <lawaliharuna943@gmail.com>

--- api/get-ufitpay-api-key.js
+++ api/get-ufitpay-api-key.js
@@ -1,22 +1,19 @@
 export default function handler(req, res) {
   if (req.method === "GET") {
     const UFITPAY_API_KEY = process.env.UFITPAY_API_KEY;
-    const UFITPAY_API_TOKEN = process.env.UFITPAY_API_TOKEN;
 
-    if (UFITPAY_API_KEY && UFITPAY_API_TOKEN) {
+    if (UFITPAY_API_KEY) {
       return res.status(200).json({ 
         success: true, 
-        apiKey: UFITPAY_API_KEY,
-        apiToken: UFITPAY_API_TOKEN 
+        apiKey: UFITPAY_API_KEY 
       });
     } else {
       console.error("Environment Variables Missing:");
       console.error("UFITPAY_API_KEY:", process.env.UFITPAY_API_KEY);
-      console.error("UFITPAY_API_TOKEN:", process.env.UFITPAY_API_TOKEN);
 
       return res.status(500).json({ 
         success: false, 
-        message: "UfitPay API credentials not found." 
+        message: "UfitPay API Key not found." 
       });
     }
   } else {
