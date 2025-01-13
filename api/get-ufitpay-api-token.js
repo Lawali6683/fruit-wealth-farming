a3e4c8a1620c971acebe43077b6c611c11783036
Haruna Lawali <lawaliharuna943@gmail.com>
2025-01-13 20:08:11 +0100

    Update get-ufitpay-api-token.js
    Signed-off-by: Haruna Lawali  <lawaliharuna943@gmail.com>

--- api/get-ufitpay-api-token.js
+++ api/get-ufitpay-api-token.js
@@ -1,28 +1,25 @@
 export default function handler(req, res) {
   if (req.method === "GET") {
-    
-    const UFITPAY_API_KEY = process.env.UFITPAY_API_KEY;
     const UFITPAY_API_TOKEN = process.env.UFITPAY_API_TOKEN;
 
-    if (UFITPAY_API_KEY && UFITPAY_API_TOKEN) {
-      // Return API Key and Token securely
+    if (UFITPAY_API_TOKEN) {
       return res.status(200).json({ 
         success: true, 
-        apiKey: UFITPAY_API_KEY,
         apiToken: UFITPAY_API_TOKEN 
       });
     } else {
-      // Return error if keys are not found
+      console.error("Environment Variables Missing:");
+      console.error("UFITPAY_API_TOKEN:", process.env.UFITPAY_API_TOKEN);
+
       return res.status(500).json({ 
         success: false, 
-        message: "UfitPay API credentials not found." 
+        message: "UfitPay API Token not found." 
       });
     }
   } else {
-    // Handle unsupported HTTP methods
-    return res.status(405).json({ 
+    return res.setHeader("Allow", ["GET"]).status(405).json({ 
       success: false, 
-      message: "Method Not Allowed." 
+      message: `Method ${req.method} Not Allowed.` 
     });
   }
 }
